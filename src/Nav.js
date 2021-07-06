3b4438cfd75e16a8ed6f9e67e0a75cfea00079b9
hamazumi <khamamoto12@gmail.com>
2021-07-06 14:21:09 -0700

    styling added

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,49 @@
-import './Nav.css'
+import styled, {css, keyframes} from 'styled-components'
+
+const fade = keyframes`
+    0% {
+        opacity: 0;
+    }
+
+    25% {
+        opacity: .5;
+    }
+
+    50%{
+        opacity: .7;
+    }
+
+    100% {
+        opacity: 1;
+    }
+`
+
+const NavHolder = styled.nav`
+    border: 2px solid white;
+    color: white;
+    text-algin: center;
+    animation: ${fade} 5s linear infinite;
+`
+
+const Header = styled.h1`
+    color: lightblue;
+    font-size: 35px;
+`
+//you can inherit other styling. In this case, Size
+const PinkHeader = styled(Header)`
+    color: purple;
+`
+
+
+
 
 export default function Nav() {
     return(
         <div>
-            <nav>
-                <h1>Hello!!</h1>
-            </nav>
+            <NavHolder>
+                <Header>Hello!!</Header>
+                <PinkHeader>Testing Pink</PinkHeader>
+            </NavHolder>
         </div>
     )
 }
