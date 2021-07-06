8ebf239890a86ea8aee56c360a247d622b6b4bf4
hamazumi <khamamoto12@gmail.com>
2021-07-06 15:18:21 -0700

    material-ui styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './Nav'
-import { Card } from 'react-bootstrap'
+import { Button, Menu, MenuItem, Fade } from '@material-ui/core'
+import {useState} from 'react'
 
 function App() {
+
+  const[menuOpen, setMenuOpen] =useState(null)
+
+  const handleClick = (e) => {
+    setMenuOpen(e.currentTarget)
+  }
+
   return (
     <div className="App">
-      <Nav />
       <header className="App-header">
-        <Card style={{ width: "120px" }}>
-          <Card.Img src={ logo } />
-          <Card.Title>Learn React!!</Card.Title>
-          <Card.Subtitle>This is a test react</Card.Subtitle>
-        </Card>
+        <Button style={{color: "white" }} aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+          Open Menu
+        </Button>
+        <Menu keepMounted open={menuOpen} TransitionComponent={Fade} anchorEl={menuOpen}>
+          <MenuItem ><a href="http://www.google.com">Option 1</a></MenuItem>
+          <MenuItem >Option 2</MenuItem>
+          <MenuItem >Option 3</MenuItem>
+        </Menu>
       </header>
     </div>
   );
