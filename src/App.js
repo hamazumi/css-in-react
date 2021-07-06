7c7e02bdeef0fbb72c356098fc8eccfa2c9e83e1
hamazumi <khamamoto12@gmail.com>
2021-07-06 14:47:11 -0700

    bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav'
-import { Card, Image, Heading } from 'rebass'
+import { Card } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
-        <Card width={120}>
-          <Image src={logo} />
-          <Heading>Learn React!!</Heading>
+        <Card style={{ width: "120px" }}>
+          <Card.Img src={ logo } />
+          <Card.Title>Learn React!!</Card.Title>
+          <Card.Subtitle>This is a test react</Card.Subtitle>
         </Card>
       </header>
     </div>
